31fb688439a90919daf019f02b2d9175f0a62301
Ashwani Mishra <developer6.afluex@gmail.com>
2023-02-20 11:01:30 +0530

    changes

---
+++ AnkurUdyogERP/Models/Distributer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AnkurUdyogERP.Models
+{
+    public class Distributer : Common
+    {
+        
+        public string FirmName { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string FatherName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string UserID { get; set; }
+        public string GSTNo { get; set; }
+        public string Limit { get; set; }
+    }
+}
