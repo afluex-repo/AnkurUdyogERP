fd3bb6ab71aa9f9a80cace2f3b486a10daef331b
Sudhakar Sharma <developer2.afluex@gmail.com>
2023-02-24 10:30:01 +0530

    changes

---
+++ AnkurUdyogERP/Models/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace AnkurUdyogERP.Models
+{
+    public class Menu
+    {
+        public string PK_MenuId { get; set; }
+        public string MenuName { get; set; }
+        public string Url { get; set; }
+        public string Sequence { get; set; }
+        public string PK_SubMenuId { get; set; }
+        public string SubMenuName { get; set; }
+        public string FK_MenuId { get; set; }
+        public List<Menu> lstMenu { get; set; }
+        public List<Menu> lstSubMenu { get; set; }
+
+        public DataSet GetMenu()
+        {
+            DataSet ds = Connection.ExecuteQuery("FindMenu");
+            return ds;
+        }
+    }
+}
